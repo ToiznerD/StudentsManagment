587d723d9c0b40b409576b759d6b5bae59076b6b
Dor Toizner <toiznerd@gmail.com>
2022-08-14 21:30:59 +0300

    start commiting!

---
+++ src/admin/StudentData.java
@@ -0,0 +1,84 @@
+package admin;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class StudentData {
+	
+	private StringProperty ID;
+	private StringProperty firstName;
+	private StringProperty lastName;
+	private StringProperty email;
+	private StringProperty DOB;
+	
+	public StudentData(String id, String firstname, String lastname, String email, 
+			String dob) {
+		this.ID = new SimpleStringProperty(id);
+		this.firstName = new SimpleStringProperty(firstname);
+		this.lastName = new SimpleStringProperty(lastname);
+		this.email = new SimpleStringProperty(email);
+		this.DOB = new SimpleStringProperty(dob);
+		
+	}
+
+	public StringProperty getID() {
+		return ID;
+	}
+
+	public void setID(StringProperty iD) {
+		ID = iD;
+	}
+
+	public StringProperty getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(StringProperty firstName) {
+		this.firstName = firstName;
+	}
+
+	public StringProperty getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(StringProperty lastName) {
+		this.lastName = lastName;
+	}
+
+	public StringProperty getEmail() {
+		return email;
+	}
+
+	public void setEmail(StringProperty email) {
+		this.email = email;
+	}
+
+	public StringProperty getDOB() {
+		return DOB;
+	}
+
+	public void setDOB(StringProperty dOB) {
+		DOB = dOB;
+	}
+	
+	public StringProperty IDProperty() {
+		return this.ID;
+	}
+	
+	public StringProperty emailProperty() {
+		return email;
+	}
+	
+	public StringProperty firstNameProperty() {
+		return this.firstName;
+	}
+	
+	public StringProperty lastNameProperty() {
+		return this.lastName;
+	}
+	
+	public StringProperty DOBProperty() {
+		return this.DOB;
+	}
+	
+}
---
+++ src/loginapp/LoginController.java
@@ -0,0 +1,117 @@
+package loginapp;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import admin.AdminController;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import students.StudentsController;
+
+public class LoginController implements Initializable{
+	
+	LoginModel loginModel = new LoginModel();
+	
+	@FXML
+	private Label dbstatus;
+	
+	@FXML
+	private TextField username;
+	
+	@FXML
+	private PasswordField password;
+	
+	@FXML
+	private ComboBox<option> combobox;
+	
+	@FXML
+	private Button loginButton;
+	
+	@FXML
+	private Label loginStatus;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		if(this.loginModel.isDatabaseConnected()) {
+			this.dbstatus.setText("Connected");
+		}
+		else
+			this.dbstatus.setText("Not Connected");
+		
+		this.combobox.setItems(FXCollections.observableArrayList(option.values()));
+	}
+	
+	@FXML
+	void Login(ActionEvent event) {
+		try {
+			if(this.loginModel.isLogin(this.username.getText(), this.password.getText(), ((option)this.combobox.getValue()).toString())) {
+				Stage stage = (Stage)this.loginButton.getScene().getWindow();
+				stage.close();
+				switch(((option)this.combobox.getValue()).toString()) {
+					case "Admin":
+						adminLogin();
+						break;
+					case "Student":
+						studentLogin();
+						break;
+				}
+			}
+			else {
+				this.loginStatus.setText("Wrong Creditials");
+			}
+		}
+		catch(Exception localException) {
+			localException.printStackTrace();
+		}
+	}
+	
+	public void studentLogin() {
+		try {
+			Stage userStage = new Stage();
+			FXMLLoader loader = new FXMLLoader();
+			Pane studentRoot = (Pane)loader.load(getClass().getResource("/students/studentFXML.fxml").openStream());
+			
+			StudentsController studentsController = (StudentsController)loader.getController();
+			
+			Scene scene = new Scene(studentRoot);
+			userStage.setScene(scene);
+			userStage.setTitle("Student Dashboard");
+			userStage.setResizable(false);
+			userStage.show();
+		}
+		catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void adminLogin() {
+		try {
+			Stage adminStage = new Stage();
+			FXMLLoader adminLoader = new FXMLLoader();
+			Pane adminroot = (Pane)adminLoader.load(getClass().getResource("/admin/Admin.fxml").openStream());
+			
+			AdminController adminController = (AdminController)adminLoader.getController();
+			
+			Scene scene = new Scene(adminroot);
+			adminStage.setScene(scene);
+			adminStage.setTitle("Admin Dashboard");
+			adminStage.setResizable(false);
+			adminStage.show();
+		}
+		catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
